8a65c5cf2c4382d9cf4a264f9de4e566513b4fcd
Jonathan Halcrow <jonathan.halcrow@gmail.com>
2011-02-12 13:58:08 -0500

    Starting a javascript version of mondrian.py for my blog

---
+++ mondrian/mondrian.js
@@ -0,0 +1,9 @@
+function draw_mondrian(canvas, budget) {
+
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "rgb(0,0,0)";
+    ctx.fillStyle = "rgb(200,0,0)"
+    
+    ctx.fillRect(10, 10, 55, 50);
+    
+}
