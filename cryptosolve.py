d6c9a234d7fb02c161d4254cab5d9cf66462fa58
Jonathan Halcrow <jonathan.halcrow@gmail.com>
2011-11-14 17:41:36 -0500

    Attempt to handle spacing

--- cryptosolve.py
+++ cryptosolve.py
@@ -1,8 +1,8 @@
 from __future__ import division
-from string import maketrans, ascii_letters, lowercase
+from string import maketrans, ascii_letters
 from collections import Counter
 from math import log, exp
-from operator import add
+from difflib import get_close_matches
 from random import randint, random
 from copy import copy
 import re
@@ -78,7 +78,7 @@ class CryptoSolver(object):
         return ''.join(guess)
 
 
-    def solve(self, crypt, guess=None, perms=1, iters=100, log_accept=log(0.1)):
+    def solve(self, crypt, words, guess=None, perms=1, iters=100, log_accept=log(0.1)):
         if guess is None:
             cur = self.guess(crypt)
         else:
@@ -102,18 +102,26 @@ class CryptoSolver(object):
                 cur = guess
                 cur_ll = guess_ll
             if cur_ll > best_ll:
-                print cur_ll
                 best_ll = cur_ll
                 best_guess = cur
+
             if n % 1000 == 0:
                 print 'Iteration %d: %f' % (n, self.likelihood(cur, crypt))
-                print '\tTemperature: %f\tAccept prob: %f' % (temperature(n, iters), P)
+                print '\tTemperature: %f\tAccept prob: %f' % (exp_temperature(n, iters), P)
                 #cur = best_guess
                 #cur_ll = best_ll
 
         print 'Final likelihood: %f' % best_ll
-        print 'Translation: %s' %  crypt.translate(maketrans(best_guess, self.chars))
+        translation = crypt.translate(maketrans(best_guess, self.chars))
+        print 'Char only guess: \n' + translation
+        spacing = space_annealing(translation, words, 5000)
+
+        print 'Best guess: %s' % ' '.join(space_text(translation, spacing))
+        print spacing
         return best_guess
+
+def space_text(letters, spacing):
+    return [letters[i:j] for (i, j) in zip([0] + spacing, spacing + [len(letters)])]
 
 def exp_temperature(it, it_max):
     return 200 * exp( -3 *  it / it_max)
@@ -121,9 +129,100 @@ def exp_temperature(it, it_max):
 def lin_temperature(it, it_max):
     return 500 * (1 - it / it_max)
 
-temperature = exp_temperature
+def spacing_init(raw, words):
+    lens = sorted(words.keys(), key=lambda k: -k)
+    blocks = []
+    for l in lens:
+        matches = [w for w in words[l] if raw.find(w) >= 0]
+        for m in matches:
+            start = raw.find(m)
+            end = start + l
+            for b0, b1, w in blocks:
+                if not (start >= b0 and start < b1) and not (end > b0 and end <= b1):
+                    blocks += [(start, end, w)]
+                
 
-def accept_prob(l0, l, it, it_max):
+def space_annealing(raw, words, iters, temp=exp_temperature):
+    cur_spacing = [i for i in xrange(len(raw)) if i % 5 == 0]
+    cur_spacing_energy = space_energy(raw, cur_spacing, words)
+    best_spacing_energy = cur_spacing_energy
+    best_spacing = cur_spacing
+    for n in xrange(iters):
+        if n % 500 == 0:
+            print 'Iteration: %d\tSpacing Energy:%f' % (n, cur_spacing_energy)
+        guess_spacing = space_guess(cur_spacing, len(raw))
+        guess_energy = space_energy(raw, guess_spacing, words)
+        if accept_prob(cur_spacing_energy*10, guess_energy*10, n, iters, temp) > random():
+            cur_spacing = guess_spacing
+            cur_spacing_energy = guess_energy
+
+        if cur_spacing_energy < best_spacing_energy:
+            best_spacing = cur_spacing
+            best_spacing_energy = cur_spacing_energy
+
+    print ' '.join(space_text(raw, best_spacing))
+    return best_spacing
+
+
+
+def space_guess(cur, max_space):
+    '''
+    Randomly either delete, add, or move a space
+    '''
+    r = random()
+    guess = copy(cur)
+    if len(cur) > 0 and r < 1/3 or len(cur) == max_space - 1: # Delete case
+        i = randint(0, len(cur) - 1)
+        guess.pop(i)
+    elif len(cur) > 0 and r > 2/3:
+        i = randint(0, len(cur) - 1)
+        if random() < 0.5:
+            guess[i] += 1
+        else:
+            guess[i] -= 1
+
+    else:
+        while True:
+            new = randint(1, max_space)
+            if new not in guess:
+                break
+        guess += [new,]
+        guess.sort()
+
+    guess = sorted(list(set(guess)))
+    return guess
+    
+def space_energy(trans, spacing, words):
+    e = 0 
+    for word in space_text(trans, spacing):
+        e += 0.1 * (len(word) - 4)**2
+        if not words.has_key(len(word)):
+            e += len(word) ** 2
+            continue
+        matches = get_close_matches(word, words[len(word)])
+        if len(matches) == 0:
+            e += len(word)
+        else:
+            e += min(map(lambda w: hamm_dist(word, w), matches))
+    return e
+
+
+def hamm_dist(a, b):
+    '''
+    Hamming distance between two strings, the number of letters that don't match
+    '''
+    if len(a) != len(b):
+        return max(len(a), len(b))
+
+    d = 0
+    for la, lb in zip(a, b):
+        if la != lb:
+            d += 1
+
+    return d
+
+
+def accept_prob(l0, l, it, it_max, temperature=exp_temperature):
     return exp(-max(0, l - l0) / temperature(it, it_max))
 
 if __name__ == '__main__':
@@ -135,6 +234,16 @@ if __name__ == '__main__':
   WzjkHJkiHJRYFiYgzdFtHuYzujgYeNYkYgzdNFNJJYtduYYeuzRuHDgHJ
   cFiHFWzjBJztWzjkHJczFiYgYFiNJRg
     '''
+    crypt = re.sub('[^A-Za-z]','', crypt)
+    #words = list(set(w.strip().lower() for w in open('/usr/share/dict/words')))
+    words = set()
+    len_count = Counter()
+    for l in open('alice.txt'):
+        line = [w.strip() for w in re.sub('[^a-z ]', ' ', l.lower()).split()]
+        len_count.update(map(len, line))
+        words.update(line)
+
+    words_by_len = {l:[w for w in words if len(w) == l] for l in xrange(3, 10)}
     solver = CryptoSolver()
     solver.train(open(sys.argv[1]).read())
-    print 'Translation %s' % solver.solve(crypt, iters=100000)
+    print 'Translation %s' % solver.solve(crypt, words_by_len, iters=100000)
